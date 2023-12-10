3d5e5117bec5126d122c64541f739e5104422f60
Slameich-web <slameacer@mail.ru>
2023-12-10 12:28:01 +0300

    test

--- src/Slider.tsx
+++ src/Slider.tsx
@@ -6,20 +6,34 @@ import { Link } from "react-router-dom";
 interface CityType {
   id: number;
   city: string;
+  img: string;
 }
 
 export const Slider = () => {
   const state: CityType[] = [
-    { id: 1, city: "Moscow" },
-    { id: 2, city: "Samara" },
+    {
+      id: 1,
+      city: "Moscow",
+      img: "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663320905_22-mykaleidoscope-ru-p-sovremennaya-moskva-oboi-24.jpg",
+    },
+    {
+      id: 2,
+      city: "Samara",
+      img: "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663320905_22-mykaleidoscope-ru-p-sovremennaya-moskva-oboi-24.jpg",
+    },
   ];
   return (
     <>
       <Swiper className="mySwiper">
-        {state.map(({ id, city }) => {
+        {state.map(({ id, city, img }) => {
           return (
             <SwiperSlide className="SwiperSlide" key={id}>
-              <Link to={`/${id}`}>{city}</Link>
+              <img alt="" src={img} />
+              <div className="SwiperSlideCity">
+                <Link className="SwiperSlideCityLink" to={`/${id}`}>
+                  dddddddddddddddd
+                </Link>
+              </div>
             </SwiperSlide>
           );
         })}
