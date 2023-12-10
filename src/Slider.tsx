393be7670498a0a2c9ac70e6e7187df5c7437fe5
Slameich-web <slameacer@mail.ru>
2023-12-10 12:05:47 +0300

    test slider

--- src/Slider.tsx
+++ src/Slider.tsx
@@ -1,3 +1,29 @@
+import { Swiper, SwiperSlide } from "swiper/react";
+
+import "swiper/css";
+import { Link } from "react-router-dom";
+
+interface CityType {
+  id: number;
+  city: string;
+}
+
 export const Slider = () => {
-  return <div>Slider</div>;
+  const state: CityType[] = [
+    { id: 1, city: "Moscow" },
+    { id: 2, city: "Samara" },
+  ];
+  return (
+    <>
+      <Swiper className="mySwiper">
+        {state.map(({ id, city }) => {
+          return (
+            <SwiperSlide className="SwiperSlide" key={id}>
+              <Link to={`/${id}`}>{city}</Link>
+            </SwiperSlide>
+          );
+        })}
+      </Swiper>
+    </>
+  );
 };
