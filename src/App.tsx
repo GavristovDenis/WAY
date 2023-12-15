ccf6ab31fb38b1cc603f7b1a4aa5190fdb577a4e
Slameich-web <slameacer@mail.ru>
2023-12-15 11:37:28 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { LangSwither } from "./LangSwither";
 import { Slider } from "./Slider";
+import { CityPage } from "./CityPage";
 
 function App() {
   return (
@@ -10,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<LangSwither />} />
         <Route path="/main" element={<Slider />} />
+        <Route path="/:id" element={<CityPage />} />
       </Routes>
     </Suspense>
   );
