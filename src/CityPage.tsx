ce64db859901c9311caea29d08d2599e1a14f681
Slameich-web <slameacer@mail.ru>
2023-12-15 11:52:58 +0300

    city

--- src/CityPage.tsx
+++ src/CityPage.tsx
@@ -1,6 +1,13 @@
-import { useParams } from "react-router-dom";
+import { Link, useParams } from "react-router-dom";
 
 export const CityPage = () => {
   const { id } = useParams();
-  return <div>{id}</div>;
+  return (
+    <div>
+      <div>
+        <Link to={"/main"}>back</Link>
+      </div>
+      {id}
+    </div>
+  );
 };
