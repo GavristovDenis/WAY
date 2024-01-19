0a60f5653e22144a984222a3e1346a97eaafe8f2
Slameich-web <slameacer@mail.ru>
2024-01-19 12:41:23 +0300

    useTranslation description

--- src/Slider.tsx
+++ src/Slider.tsx
@@ -8,6 +8,7 @@ interface CityType {
   id: number;
   city: string;
   img: string;
+  description: string;
 }
 
 export const Slider = () => {
@@ -17,26 +18,31 @@ export const Slider = () => {
       id: 1,
       city: t("Moscow"),
       img: "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663320905_22-mykaleidoscope-ru-p-sovremennaya-moskva-oboi-24.jpg",
+      description: t("Moscow_description"),
     },
     {
       id: 2,
       city: t("Samara"),
       img: "https://sportishka.com/uploads/posts/2022-11/1667573131_35-sportishka-com-p-istoricheskie-dostoprimechatelnosti-samari-35.jpg",
+      description: t("Samara_description"),
     },
     {
       id: 3,
       city: t("Saint_Petersburg"),
       img: "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663345945_30-mykaleidoscope-ru-p-vidi-sankt-peterburga-krasivo-33.jpg",
+      description: t("Saint_Petersburg_description"),
     },
     {
       id: 4,
       city: t("Vladivostok"),
       img: "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663419347_5-mykaleidoscope-ru-p-vladivostok-stolitsa-dalnego-vostoka-vkont-5.jpg",
+      description: t("Vladivostok_description"),
     },
     {
       id: 5,
       city: t("Crimea"),
       img: "https://sportishka.com/uploads/posts/2022-03/1646969368_51-sportishka-com-p-kukushkino-gnezdo-krim-turizm-krasivo-foto-56.jpg",
+      description: t("Crimea_description"),
     },
   ];
   return (
