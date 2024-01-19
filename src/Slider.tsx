f6241ae538709ee3eb2909287738e2a2ff3da606
Slameich-web <slameacer@mail.ru>
2024-01-19 12:27:04 +0300

    useTranslation

--- src/Slider.tsx
+++ src/Slider.tsx
@@ -2,6 +2,7 @@ import { Swiper, SwiperSlide } from "swiper/react";
 
 import "swiper/css";
 import { Link } from "react-router-dom";
+import { useTranslation } from "react-i18next";
 
 interface CityType {
   id: number;
@@ -10,16 +11,32 @@ interface CityType {
 }
 
 export const Slider = () => {
+  const { t } = useTranslation();
   const state: CityType[] = [
     {
       id: 1,
-      city: "Moscow",
+      city: t("Moscow"),
       img: "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663320905_22-mykaleidoscope-ru-p-sovremennaya-moskva-oboi-24.jpg",
     },
     {
       id: 2,
-      city: "Samara",
-      img: "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663320905_22-mykaleidoscope-ru-p-sovremennaya-moskva-oboi-24.jpg",
+      city: t("Samara"),
+      img: "https://sportishka.com/uploads/posts/2022-11/1667573131_35-sportishka-com-p-istoricheskie-dostoprimechatelnosti-samari-35.jpg",
+    },
+    {
+      id: 3,
+      city: t("Saint_Petersburg"),
+      img: "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663345945_30-mykaleidoscope-ru-p-vidi-sankt-peterburga-krasivo-33.jpg",
+    },
+    {
+      id: 4,
+      city: t("Vladivostok"),
+      img: "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663419347_5-mykaleidoscope-ru-p-vladivostok-stolitsa-dalnego-vostoka-vkont-5.jpg",
+    },
+    {
+      id: 5,
+      city: t("Crimea"),
+      img: "https://sportishka.com/uploads/posts/2022-03/1646969368_51-sportishka-com-p-kukushkino-gnezdo-krim-turizm-krasivo-foto-56.jpg",
     },
   ];
   return (
