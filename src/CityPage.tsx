6b2c6d52cfe87f47dd113b6f72f3731938fe61a2
Slameich-web <slameacer@mail.ru>
2024-01-22 10:28:10 +0300

    test city

--- src/CityPage.tsx
+++ src/CityPage.tsx
@@ -1,13 +1,18 @@
 import { Link, useParams } from "react-router-dom";
+import { useCityStore } from "./store";
 
 export const CityPage = () => {
   const { id } = useParams();
+  const state = useCityStore((state) => state.city);
+  const filteredItems = state.filter((item) =>
+    item.id.toString().includes(String(id))
+  );
   return (
     <div>
       <div>
         <Link to={"/main"}>back</Link>
       </div>
-      {id}
+      {filteredItems[0].city}
     </div>
   );
 };
--- src/store.ts
+++ src/store.ts
@@ -5,6 +5,7 @@ interface CityType {
   city: string;
   img: string;
   description: string;
+  progress: number;
 }
 interface BearState {
   city: CityType[];
@@ -18,30 +19,35 @@ export const useCityStore = create<BearState>()((set) => ({
       city: t("Moscow"),
       img: "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663320905_22-mykaleidoscope-ru-p-sovremennaya-moskva-oboi-24.jpg",
       description: t("Moscow_description"),
+      progress: 100,
     },
     {
       id: 2,
       city: t("Samara"),
       img: "https://sportishka.com/uploads/posts/2022-11/1667573131_35-sportishka-com-p-istoricheskie-dostoprimechatelnosti-samari-35.jpg",
       description: t("Samara_description"),
+      progress: 50,
     },
     {
       id: 3,
       city: t("Saint_Petersburg"),
       img: "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663345945_30-mykaleidoscope-ru-p-vidi-sankt-peterburga-krasivo-33.jpg",
       description: t("Saint_Petersburg_description"),
+      progress: 0,
     },
     {
       id: 4,
       city: t("Vladivostok"),
       img: "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663419347_5-mykaleidoscope-ru-p-vladivostok-stolitsa-dalnego-vostoka-vkont-5.jpg",
       description: t("Vladivostok_description"),
+      progress: 0,
     },
     {
       id: 5,
       city: t("Crimea"),
       img: "https://sportishka.com/uploads/posts/2022-03/1646969368_51-sportishka-com-p-kukushkino-gnezdo-krim-turizm-krasivo-foto-56.jpg",
       description: t("Crimea_description"),
+      progress: 0,
     },
   ],
   increase: (by) => set((state) => ({ city: state.city })),
