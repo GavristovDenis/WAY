6398af568387014aaeb492044d03213db4cd2112
Slameich-web <slameacer@mail.ru>
2024-01-22 11:18:56 +0300

    test city

--- src/store.ts
+++ src/store.ts
@@ -1,4 +1,3 @@
-import { t } from "i18next";
 import { create } from "zustand";
 interface CityType {
   id: number;
@@ -16,37 +15,37 @@ export const useCityStore = create<BearState>()((set) => ({
   city: [
     {
       id: 1,
-      city: t("Moscow"),
+      city: "Moscow",
       img: "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663320905_22-mykaleidoscope-ru-p-sovremennaya-moskva-oboi-24.jpg",
-      description: t("Moscow_description"),
+      description: "Moscow_description",
       progress: 100,
     },
     {
       id: 2,
-      city: t("Samara"),
+      city: "Samara",
       img: "https://sportishka.com/uploads/posts/2022-11/1667573131_35-sportishka-com-p-istoricheskie-dostoprimechatelnosti-samari-35.jpg",
-      description: t("Samara_description"),
+      description: "Samara_description",
       progress: 50,
     },
     {
       id: 3,
-      city: t("Saint_Petersburg"),
+      city: "Saint_Petersburg",
       img: "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663345945_30-mykaleidoscope-ru-p-vidi-sankt-peterburga-krasivo-33.jpg",
-      description: t("Saint_Petersburg_description"),
+      description: "Saint_Petersburg_description",
       progress: 0,
     },
     {
       id: 4,
-      city: t("Vladivostok"),
+      city: "Vladivostok",
       img: "https://mykaleidoscope.ru/x/uploads/posts/2022-09/1663419347_5-mykaleidoscope-ru-p-vladivostok-stolitsa-dalnego-vostoka-vkont-5.jpg",
-      description: t("Vladivostok_description"),
+      description: "Vladivostok_description",
       progress: 0,
     },
     {
       id: 5,
-      city: t("Crimea"),
+      city: "Crimea",
       img: "https://sportishka.com/uploads/posts/2022-03/1646969368_51-sportishka-com-p-kukushkino-gnezdo-krim-turizm-krasivo-foto-56.jpg",
-      description: t("Crimea_description"),
+      description: "Crimea_description",
       progress: 0,
     },
   ],
