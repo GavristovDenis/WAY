fd1733a7bc5ee793f7184bac7a11ed80de07fcf7
Slameich-web <slameacer@mail.ru>
2024-06-10 22:14:54 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   const tg = window.Telegram.WebApp;
   return (
     <Suspense fallback="loading">
-      {tg.initDataUnsafe}
+      {tg.initDataUnsafe.user.username}
       <Routes>
         <Route path="/aaa" element={<LoadingPage />} />
         <Route path="/aaa" element={<Sidebar />} />
