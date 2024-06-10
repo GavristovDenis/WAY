dd1843e2686fb3f2fd591463fca642afe5cebbe6
Slameich-web <slameacer@mail.ru>
2024-06-10 20:56:26 +0300

    Help WAY-6

--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,16 @@ import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { LoadingPage } from "./LoadingPage/LoadingPage";
 import { Sidebar } from "./Sidebar/Sidebar";
+import { Help } from "./Help/Help";
 
 function App() {
   return (
     <Suspense fallback="loading">
       <Routes>
         <Route path="/aaa" element={<LoadingPage />} />
-        <Route path="/" element={<Sidebar />} />
+        <Route path="/aaa" element={<Sidebar />} />
+
+        <Route path="/" element={<Help />} />
       </Routes>
     </Suspense>
   );
