1663129697d4a0b83073e20f9cdb71f12ce243f9
Slameich-web <slameacer@mail.ru>
2024-06-10 22:20:42 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -1,16 +1,20 @@
 import "./App.scss";
-import { Suspense } from "react";
+import { Suspense, useEffect, useState } from "react";
 import { Route, Routes } from "react-router-dom";
 import { LoadingPage } from "./LoadingPage/LoadingPage";
 import { Sidebar } from "./Sidebar/Sidebar";
 import { Help } from "./Help/Help";
 
 function App() {
-  //@ts-ignore
-  const tg = window.Telegram.WebApp;
+  const [state, setState] = useState();
+  useEffect(() => {
+    //@ts-ignore
+    setState(window.Telegram.WebApp.initDataUnsafe);
+  });
+
   return (
     <Suspense fallback="loading">
-      {JSON.stringify(tg.initDataUnsafe)}
+      {JSON.stringify(state)}
       <Routes>
         <Route path="/aaa" element={<LoadingPage />} />
         <Route path="/aaa" element={<Sidebar />} />
