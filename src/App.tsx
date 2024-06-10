f1d7a947ddf551d2d788ab8bdcf6270b84b5861c
Slameich-web <slameacer@mail.ru>
2024-06-10 22:28:10 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -7,14 +7,17 @@ import { Help } from "./Help/Help";
 
 function App() {
   const [state, setState] = useState();
+  const [stateq, setStateq] = useState(1);
+
   useEffect(() => {
     //@ts-ignore
     setState(window.Telegram);
-  });
+  }, [stateq]);
 
   return (
     <Suspense fallback="loading">
       {JSON.stringify(state)}
+      <div onClick={() => setStateq((prev) => prev + 1)}>xxx</div>
       <Routes>
         <Route path="/aaa" element={<LoadingPage />} />
         <Route path="/aaa" element={<Sidebar />} />
