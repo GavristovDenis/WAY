32c3a7c94948d1ffea50330c795da6bc7db7a508
Slameich-web <slameacer@mail.ru>
2024-06-10 19:49:42 +0300

    new version WAY-5

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,13 @@
 import "./App.scss";
 import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
-import { LangSwither } from "./LangSwither/LangSwither";
-import { Slider } from "./Slider";
-import { CityPage } from "./CityPage";
-import { Itinerary } from "./Itinerary";
-import { Road } from "./Road";
+import { LoadingPage } from "./LoadingPage/LoadingPage";
 
 function App() {
   return (
     <Suspense fallback="loading">
       <Routes>
-        <Route path="/" element={<LangSwither />} />
-        <Route path="/main" element={<Slider />} />
-        <Route path="/:id" element={<CityPage />} />
-        <Route path="/itinerary/:id" element={<Itinerary />} />
-        <Route path="/itinerary/:id/:id" element={<Road />} />
+        <Route path="/" element={<LoadingPage />} />
       </Routes>
     </Suspense>
   );
