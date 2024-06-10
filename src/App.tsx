bfb026c1280222b4527e9ed0172dd269b93c2eb0
Slameich-web <slameacer@mail.ru>
2024-06-10 20:24:57 +0300

    Sidebar WAY-2

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import "./App.scss";
 import { Suspense } from "react";
 import { Route, Routes } from "react-router-dom";
 import { LoadingPage } from "./LoadingPage/LoadingPage";
+import { Sidebar } from "./Sidebar/Sidebar";
 
 function App() {
   return (
     <Suspense fallback="loading">
       <Routes>
         <Route path="/aaa" element={<LoadingPage />} />
-        <Route path="/" element={<LoadingPage />} />
+        <Route path="/" element={<Sidebar />} />
       </Routes>
     </Suspense>
   );
