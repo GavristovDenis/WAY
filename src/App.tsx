06053b800d7a812b77854aedba9da636ff2a6dff
Slameich-web <slameacer@mail.ru>
2024-06-15 02:43:42 +0300

    test

--- src/App.tsx
+++ src/App.tsx
@@ -13,17 +13,18 @@ function App() {
   // const location = useLocation();
   return (
     <Suspense fallback="loading">
-      {/* {location.pathname === "/" ? null : <Header />} */}
-      <Header />
-      <Routes>
-        <Route path="/" element={<LoadingPage />} />
-        <Route path="/sidebar" element={<Sidebar />} />
-        <Route path="/events" element={<Events />} />
-        <Route path="/places" element={<Places />} />
-        <Route path="/events/:id" element={<CityItem />} />
-        <Route path="/places/:id" element={<CityItem />} />
-        <Route path="/help" element={<Help />} />
-      </Routes>
+      <div>
+        <Header />
+        <Routes>
+          <Route path="/" element={<LoadingPage />} />
+          <Route path="/sidebar" element={<Sidebar />} />
+          <Route path="/events" element={<Events />} />
+          <Route path="/places" element={<Places />} />
+          <Route path="/events/:id" element={<CityItem />} />
+          <Route path="/places/:id" element={<CityItem />} />
+          <Route path="/help" element={<Help />} />
+        </Routes>
+      </div>
     </Suspense>
   );
 }
