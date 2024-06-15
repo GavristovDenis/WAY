802556513df04d15bd9ed33ce18e20954e0a17c7
Slameich-web <slameacer@mail.ru>
2024-06-15 20:55:11 +0300

    test map

--- src/const/mockData.tsx
+++ src/const/mockData.tsx
@@ -17,7 +17,8 @@ export const placesData: ListData[] = [
     ],
     author: "Марии Дубцовой",
     date: "24 апреля, 2024",
-    mapLink: "yandexmaps://maps.yandex.ru/?pt=37.673098,55.757134&z=12&l=map",
+    mapLink:
+      "yandexmaps://maps.yandex.ru/maps/org/ges_2/216491468916/?ll=37.612731%2C55.742651&z=16.71",
   },
   {
     id: 2,
@@ -34,7 +35,8 @@ export const placesData: ListData[] = [
     ],
     author: "Марии Дубцовой",
     date: "24 апреля, 2024",
-    mapLink: "yandexmaps://maps.yandex.ru/?pt=37.673098,55.757134&z=12&l=map",
+    mapLink:
+      "yandexmaps://maps.yandex.ru/maps/org/ges_2/216491468916/?ll=37.612731%2C55.742651&z=16.71",
   },
 
   {
@@ -51,7 +53,8 @@ export const placesData: ListData[] = [
     ],
     author: "Марии Дубцовой",
     date: "24 апреля, 2024",
-    mapLink: "yandexmaps://maps.yandex.ru/?pt=37.673098,55.757134&z=12&l=map",
+    mapLink:
+      "yandexmaps://maps.yandex.ru/maps/org/ges_2/216491468916/?ll=37.612731%2C55.742651&z=16.71",
   },
 ];
 
@@ -71,7 +74,8 @@ export const eventsData = [
     ],
     author: "Марии Дубцовой",
     date: "24 апреля, 2024",
-    mapLink: "yandexmaps://maps.yandex.ru/?pt=37.673098,55.757134&z=12&l=map",
+    mapLink:
+      "yandexmaps://maps.yandex.ru/maps/org/ges_2/216491468916/?ll=37.612731%2C55.742651&z=16.71",
   },
   {
     id: 2,
@@ -87,7 +91,8 @@ export const eventsData = [
     ],
     author: "Марии Дубцовой",
     date: "24 апреля, 2024",
-    mapLink: "yandexmaps://maps.yandex.ru/?pt=37.673098,55.757134&z=12&l=map",
+    mapLink:
+      "yandexmaps://maps.yandex.ru/maps/org/ges_2/216491468916/?ll=37.612731%2C55.742651&z=16.71",
   },
   {
     id: 3,
@@ -103,6 +108,7 @@ export const eventsData = [
     ],
     author: "Марии Дубцовой",
     date: "24 апреля, 2024",
-    mapLink: "yandexmaps://maps.yandex.ru/?pt=37.673098,55.757134&z=12&l=map",
+    mapLink:
+      "yandexmaps://maps.yandex.ru/maps/org/ges_2/216491468916/?ll=37.612731%2C55.742651&z=16.71",
   },
 ];
