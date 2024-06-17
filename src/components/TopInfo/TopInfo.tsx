0ccc5ecf9fbe4577a513b13adf1a27f0ce021fbb
Slameich-web <slameacer@mail.ru>
2024-06-17 14:49:17 +0300

    test map

--- src/components/TopInfo/TopInfo.tsx
+++ src/components/TopInfo/TopInfo.tsx
@@ -1,11 +1,24 @@
 import "./TopInfo.scss";
 import { useLocation } from "react-router-dom";
 import { StarIcon } from "../../svg/Star";
+import { useEffect, useState } from "react";
 const TopInfo = () => {
   const location = useLocation();
+  const [title, setTitle] = useState("");
+
+  useEffect(() => {
+    switch (location.pathname) {
+      case "/places":
+        setTitle("Места");
+        break;
+      case "/events":
+        setTitle("Москва");
+        break;
+    }
+  }, [location.pathname]);
   return (
     <div className="Places_top_info">
-      <div className="Top_info_text">Москва</div>
+      <div className="Top_info_text">{title}</div>
       {location.pathname === "/places" || location.pathname === "/events" ? (
         <div className="Top_info_star">
           <StarIcon />
